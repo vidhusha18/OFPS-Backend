b51be61b03401b450e8332d88b590d33968c8792
vidhusha18 <115626945+vidhusha18@users.noreply.github.com>
2024-09-19 11:46:12 +0530

    Backend files added

---
+++ src/test/java/com/vidhu/fir/FirMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhu.fir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
